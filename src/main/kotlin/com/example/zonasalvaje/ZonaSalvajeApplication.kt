899cbdff7e88a1dba22b13d12e5a41049abe49d4
Pablo Valdesoiro Pulido <pvaldesoiro@digitalorigin.com>
2018-06-27 20:02:05 +0200

    First commit

---
+++ src/main/kotlin/com/example/zonasalvaje/ZonaSalvajeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zonasalvaje
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZonaSalvajeApplication
+
+fun main(args: Array<String>) {
+  runApplication<ZonaSalvajeApplication>(*args)
+}
